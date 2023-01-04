1c727cc03eb0176b88a178b881a6598e8c2f2412
tbiggs29 <106698732+tbiggs29@users.noreply.github.com>
2023-01-04 13:45:41 -0700

    Update CANDataPlotter.py

--- CANDataPlotter.py
+++ CANDataPlotter.py
@@ -1,7 +1,8 @@
-from CANDataSPNDecoder import CANDataSPNDecode
-import matplotlib.pyplot as plt
 import sys
+import matplotlib.pyplot as plt
+from CANDataSPNDecoder import CANDataSPNDecode
 
+#Get User Input
 file = ""
 jsonFile = ""
 for i, arg in enumerate(sys.argv):
@@ -10,15 +11,25 @@ for i, arg in enumerate(sys.argv):
     elif(i==2):
         jsonFile = arg
 
+#PGN and SPN 
 pgn = "61444"
 spn = "190"
 
+#Decode candump log File
 decodedData = CANDataSPNDecode(file,  jsonFile)
 rpmData = decodedData[pgn][spn]["data"]
-timeData = decodedData[pgn][spn]["time"]
 
+#Checks if timestamps are in the data
+#If not, then it makes the x axis the number of data points
+if("time" in decodedData[pgn][spn]):
+    timeData = decodedData[pgn][spn]["time"]
+
+#Plot RPM Data
 plt.title(decodedData[pgn][spn]["SPLabel"]) 
-plt.xlabel("seconds")
-plt.ylabel(decodedData[pgn][spn]["Unit"])
-plt.plot(timeData, rpmData)
+if("time" in decodedData[pgn][spn]):
+    plt.xlabel("seconds")
+    plt.plot(timeData, rpmData)
+else:
+    plt.ylabel(decodedData[pgn][spn]["Unit"])
+    plt.plot(rpmData)
 plt.show()
